bbb96d0e282c8eed4d3f15edba0303febdd727dc
eduardodelduqui <66086087+eduardodelduqui@users.noreply.github.com>
2020-10-05 11:00:20 -0300

    Adicionado métodos e atributos

--- Projeto/pedido.py
+++ Projeto/pedido.py
@@ -1,11 +1,26 @@
-from produto import*
-from loja import*
-from cliente import*
+from categoria import Categoria
+from loja import Loja
+from cliente import Cliente
+from produto import Produto
+from datetime import datetime, date
+import uuid
 
 class Pedido:
     
-    def __init__(self,codigo: int,loja: Loja,cliente: Cliente,produto: Produto,horario):
-        self.__codigo = codigo_pedido
+    def __init__(self,loja: Loja,cliente: Cliente, produto: Produto):
+
+        self.__codigo = uuid.uuid4().hex
+        self.__produto = produto
         self.__loja = loja
-        self.__cliente = client
-        self.horario
+        self.__cliente = cliente
+        self.__data = date.today
+        self.__horario = datetime.now().strftime("%H:%M:%S")
+    
+    def imprime(self):
+
+        return dict(pedido = self.__codigo,
+                    loja = self.__loja.nome,
+                    cliente = self.__cliente.nome,
+                    produto = self.__produto.nome,
+                    data = self.__data,
+                    horario = self.__horario)
